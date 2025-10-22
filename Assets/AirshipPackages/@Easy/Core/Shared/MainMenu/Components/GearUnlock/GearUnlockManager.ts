0248581020b024606991f531930914217fd07684
Luke Chatton <chattonluke@gmail.com>
2025-10-22 16:26:28 -0700

    more polish

--- Assets/AirshipPackages/@Easy/Core/Shared/MainMenu/Components/GearUnlock/GearUnlockManager.ts
+++ Assets/AirshipPackages/@Easy/Core/Shared/MainMenu/Components/GearUnlock/GearUnlockManager.ts
@@ -1,6 +1,18 @@
 import { CoreContext } from "@Easy/Core/Shared/CoreClientContext";
 import { Game } from "@Easy/Core/Shared/Game";
+import { GameCoordinatorClient } from "@Easy/Core/Shared/TypePackages/game-coordinator-types";
+import { UnityMakeRequest } from "@Easy/Core/Shared/TypePackages/UnityMakeRequest";
+import { Keyboard } from "@Easy/Core/Shared/UserInput";
+import { AirshipUrl } from "@Easy/Core/Shared/Util/AirshipUrl";
 import GearUnlockUI from "./GearUnlockUI";
+
+const client = new GameCoordinatorClient(UnityMakeRequest(AirshipUrl.GameCoordinator));
+
+interface GearUnlockNotificationData {
+	classId: string;
+	messageTitle: string;
+	messageBody: string;
+}
 
 export default class GearUnlockManager extends AirshipBehaviour {
 	public container: GameObject;
@@ -13,25 +25,50 @@ export default class GearUnlockManager extends AirshipBehaviour {
 			return;
 		}
 
-		this.ShowRewardYielding("8710579e-6ab5-4122-a7eb-4cffe842e114");
+		if (Game.IsEditor()) {
+			Keyboard.OnKeyDown(Key.P, (e) => {
+				if (e.IsCancelled()) return;
+
+				this.ShowRewardYielding("test", {
+					classId: "ffbc21f2-f977-4452-bb98-43064890f0b8",
+					messageTitle: "Unlocked BETA Chain",
+					messageBody:
+						"Thank you for playing the Airship Beta. As a thank you, enjoy this exclusive avatar item.",
+				});
+			});
+		}
+
+		this.CheckNotifications();
 	}
 
-	public ShowRewardYielding(gearClassId: string): void {
+	public async CheckNotifications(): Promise<void> {
+		const { notifications } = await client.userNotifications.getNotifications();
+		for (let notif of notifications) {
+			if (notif.type === "GEAR_UNLOCK_POPUP") {
+				const data = notif.data as GearUnlockNotificationData;
+				this.ShowRewardYielding(notif.id, data);
+			}
+		}
+	}
+
+	public ShowRewardYielding(notificationId: string, data: GearUnlockNotificationData): void {
 		if (!Game.playerFlags.has("PlatformGearDownloadClassId")) {
 			warn("Missing flags for platform gear download.");
 			return;
 		}
 
 		print("Downloading reward gear...");
-		const gear = PlatformGear.DownloadYielding(gearClassId);
+		const gear = PlatformGear.DownloadYielding(data.classId);
 		if (!gear) {
 			warn("Downloaded gear unlock was undefined");
 			return;
 		}
 
 		print("Downloaded! Showing now...");
+		this.container.SetActive(false);
+		this.ui.Init(notificationId);
 		this.container.SetActive(true);
-		this.ui.SetGear(gear);
+		this.ui.SetGear(gear, data.messageTitle, data.messageBody);
 	}
 
 	override OnDestroy(): void {}
--- Assets/AirshipPackages/@Easy/Core/Shared/MainMenu/Components/GearUnlock/GearUnlockUI.ts
+++ Assets/AirshipPackages/@Easy/Core/Shared/MainMenu/Components/GearUnlock/GearUnlockUI.ts
@@ -1,5 +1,11 @@
 import { Game } from "@Easy/Core/Shared/Game";
+import { TweenEasingFunction } from "@Easy/Core/Shared/Tween/EasingFunctions";
+import { GameCoordinatorClient } from "@Easy/Core/Shared/TypePackages/game-coordinator-types";
+import { UnityMakeRequest } from "@Easy/Core/Shared/TypePackages/UnityMakeRequest";
+import { AirshipUrl } from "@Easy/Core/Shared/Util/AirshipUrl";
 import { Bin } from "@Easy/Core/Shared/Util/Bin";
+
+const client = new GameCoordinatorClient(UnityMakeRequest(AirshipUrl.GameCoordinator));
 
 export default class GearUnlockUI extends AirshipBehaviour {
 	public camera: Camera;
@@ -11,12 +17,26 @@ export default class GearUnlockUI extends AirshipBehaviour {
 
 	public continueBtn: Button;
 	public verticalList: VerticalLayoutGroup;
+	public titleText: TMP_Text;
+	public messageText: TMP_Text;
+	public messageLayoutElement: LayoutElement;
+	public wrapper: RectTransform;
+
+	@Header("Dummy")
+	public dummy: Transform;
+	public dummyBodyMesh: SkinnedMeshRenderer;
 
 	private targetGearCenter = Vector3.zero;
 	private bin = new Bin();
+	private openTime = 0;
 
 	override OnEnable(): void {
+		this.openTime = Time.time;
+		this.wrapper.localScale = Vector3.one.mul(1.1);
+		NativeTween.LocalScale(this.wrapper, Vector3.one, 0.2).SetEaseQuadOut();
+
 		if (this.renderTexture) {
+			this.camera.targetTexture = undefined as unknown as RenderTexture;
 			this.renderTexture.Release();
 			Destroy(this.renderTexture);
 		}
@@ -25,52 +45,95 @@ export default class GearUnlockUI extends AirshipBehaviour {
 		this.camera.targetTexture = this.renderTexture;
 		this.rawImage.texture = this.renderTexture;
 
-		this.bin.Add(
-			this.continueBtn.onClick.Connect(() => {
-				this.gameObject.SetActive(false);
-			}),
-		);
-
 		if (Game.deviceType !== AirshipDeviceType.Phone) {
 			const rect = this.verticalList.transform as RectTransform;
 			rect.anchoredPosition = new Vector2(0, 150);
+
+			this.messageLayoutElement.preferredWidth = 700;
 		}
+	}
+
+	public Init(gearNotificationId: string): void {
+		this.bin.Add(
+			this.continueBtn.onClick.Connect(() => {
+				task.spawnDetached(async () => {
+					await client.userNotifications.deleteNotifications({
+						notificationIds: [gearNotificationId],
+					});
+				});
+				this.gameObject.SetActive(false);
+			}),
+		);
 	}
 
 	protected OnDisable(): void {
 		this.bin.Clean();
 		if (this.renderTexture) {
+			this.camera.targetTexture = undefined as unknown as RenderTexture;
 			this.renderTexture.Release();
 			Destroy(this.renderTexture);
 		}
 	}
 
-	public SetGear(gear: PlatformGear): void {
+	public SetGear(gear: PlatformGear, title: string, message: string): void {
+		this.titleText.text = title;
+		this.messageText.text = message;
+
 		this.rawImage.enabled = true;
 		this.targetGearParent.gameObject.ClearChildren();
 		const acc = Instantiate(gear.accessoryPrefabs[0], this.targetGearParent);
 		acc.gameObject.SetLayerRecursive(17);
 		acc.transform.localPosition = Vector3.zero;
-		acc.transform.localRotation = Quaternion.Euler(-90, 0, 0);
+		acc.transform.localRotation = Quaternion.Euler(-90, 70, 0);
 		this.accessory = acc;
+		this.dummy.localRotation = Quaternion.Euler(0, 250, 0);
 
-		const meshRenderer = this.accessory.gameObject.GetComponentInChildren<MeshRenderer>();
-		if (meshRenderer) {
-			this.targetGearCenter = meshRenderer.bounds.center;
-		} else {
-			const skinnedMeshRenderer = this.accessory.gameObject.GetComponentInChildren<SkinnedMeshRenderer>();
-			if (skinnedMeshRenderer) {
-				this.targetGearCenter = skinnedMeshRenderer.bounds.center;
+		const meshRenderers = this.accessory.gameObject.GetComponentsInChildren<MeshRenderer>();
+		if (meshRenderers.size() > 0) {
+			this.targetGearCenter = meshRenderers[0].bounds.center;
+
+			for (let meshRenderer of meshRenderers) {
+				for (let mat of meshRenderer.sharedMaterials) {
+					if (!mat.shader.isSupported) {
+						mat.shader = Shader.Find("Universal Render Pipeline/Lit");
+					}
+				}
+			}
+		}
+
+		const skinnedMeshRenderers = this.accessory.gameObject.GetComponentsInChildren<SkinnedMeshRenderer>();
+		if (skinnedMeshRenderers.size() > 0) {
+			this.targetGearCenter = skinnedMeshRenderers[0].bounds.center;
+
+			for (let smr of skinnedMeshRenderers) {
+				smr.rootBone = this.dummyBodyMesh.rootBone;
+				smr.bones = this.dummyBodyMesh.bones;
+
+				for (let mat of smr.sharedMaterials) {
+					if (!mat.shader.isSupported) {
+						mat.shader = Shader.Find("Universal Render Pipeline/Lit");
+					}
+				}
 			}
 		}
 	}
 
 	protected Update(dt: number): void {
 		const cameraTransform = this.camera.transform;
-		cameraTransform.position = this.targetGearCenter.add(new Vector3(0, 0, -3.5));
+
+		let zOffset = 0;
+		let timeSinceStart = Time.time - this.openTime;
+		if (timeSinceStart <= 1) {
+			zOffset = TweenEasingFunction.OutQuad(timeSinceStart, 1, -1, 1);
+		}
+		cameraTransform.position = this.targetGearCenter.add(new Vector3(0, 0, -3.5 - zOffset));
 		cameraTransform.LookAt(this.targetGearCenter);
 
+		// Static Renderers
 		this.targetGearParent.RotateAround(this.targetGearCenter, Vector3.up, -35 * dt);
+
+		// Skinned Mesh Renderers
+		this.dummy.RotateAround(this.targetGearCenter, Vector3.up, -35 * dt);
 	}
 
 	override OnDestroy(): void {}
