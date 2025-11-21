89e9c377e0fc81bbd243f710d02fd920577f076f
Luke Chatton <chattonluke@gmail.com>
2025-11-21 11:06:47 -0800

    better mobile navbar

--- Assets/AirshipPackages/@Easy/Core/Shared/MainMenu/Components/MobileBottomNav.ts
+++ Assets/AirshipPackages/@Easy/Core/Shared/MainMenu/Components/MobileBottomNav.ts
@@ -1,4 +1,4 @@
-export default class MobileNavbar extends AirshipBehaviour {
+export default class MobileBottomNav extends AirshipBehaviour {
 	override Start(): void {
 		const rect = this.transform as RectTransform;
 		rect.offsetMax = new Vector2(0, Screen.safeArea.yMin / 2 + 65);
